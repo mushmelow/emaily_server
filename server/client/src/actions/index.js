f89802f284ff1adf57d126c26ff1fe65ffb29a1f
hao zheng <hao.zheng@etinet.com>
2018-08-13 14:15:11 -0400

    payment

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import {FETCH_USER} from "./types";
 
-const fetchUser = ()=>{
-    axios.get('/api/current_user')
+export const fetchUser = ()=>{
+    return function(dispatch){
+        axios
+            .get('/api/current_user')
+            .then(res=> dispatch({type: FETCH_USER, payload: res.data}))
+    }
+};
+
+export const handleToken= (token) =>{
+    return function(dispatch){
+        axios
+            .post('/api/stripe',token)
+            .then(res=> dispatch({type: FETCH_USER, payload: res.data}))
+    }
 };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,20 +1,19 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDom from 'react-dom';
 //module needed for redux
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 //import materializeCss
-import 'materialize-css/dist/css/materialize.min.css'
 
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 
 //import reducers
 import reducers from './reducers'
-import reduxThunk from 'redux-thunk';
+
 const store= createStore(reducers, {},applyMiddleware(reduxThunk));
-
-
 
 ReactDom.render(
     <Provider store={store}>
@@ -23,3 +22,4 @@ ReactDom.render(
     document.querySelector('#root')
 );
 
+console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
